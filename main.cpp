b0e6c2aa24dbdb42d94d14dce88865cc4898c6ee
Londre Blocker <londre.blocker@gmail.com>
2017-11-26 15:19:13 -0500

    Boost optional example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <boost/optional.hpp>
+
+using namespace std;
+using namespace boost;
+
+class Person {
+public:
+    Person(string firstName, string lastName) : firstName(std::move(firstName)), lastName(std::move(lastName)) {}
+
+    string firstName, lastName;
+    optional<string> middleName;
+};
+
+int main(int argc, char *argv[]) {
+
+    Person p { "John", "Watson" };
+    p.middleName = "Hamish";
+
+    if (p.middleName)
+        cout << *p.middleName << endl;
+
+    getchar();
     return 0;
 }
