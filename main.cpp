134bb20b7516923cef91822d360c7e366806ec44
Londre Blocker <londre.blocker@gmail.com>
2017-11-26 22:55:50 -0500

    Add boost library using statements

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace boost::filesystem;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
